f312091c1db2b5ce1017dea816c6078258f5244f
Daria Czerniawko <daria@czerniawko.pl>
2020-07-15 13:24:51 +0200

    fixed bug

--- do-debiutów/js/main.js
+++ do-debiutów/js/main.js
@@ -8,10 +8,15 @@ const spanSecond = document.querySelector('span.second');
 setInterval(() => {
     const nowTime = new Date().getTime();
     const time = endTime - nowTime;
-    const days = Math.floor(endTime / (1000 * 60 * 60 * 24) - (nowTime / (1000 * 60 * 60 * 24)));
-    const hours = Math.floor((endTime / (1000 * 60 * 60) - nowTime / (1000 * 60 * 60)) % 24);
-    const minutes = Math.floor((endTime / (1000 * 60) - nowTime / (1000 * 60)) % 60);
-    const seconds = Math.floor((endTime / 1000 - nowTime / 1000) % 60);
+    let days = Math.floor(endTime / (1000 * 60 * 60 * 24) - (nowTime / (1000 * 60 * 60 * 24)));
+    let hours = Math.floor((endTime / (1000 * 60 * 60) - nowTime / (1000 * 60 * 60)) % 24);
+    let minutes = Math.floor((endTime / (1000 * 60) - nowTime / (1000 * 60)) % 60);
+    let seconds = Math.floor((endTime / 1000 - nowTime / 1000) % 60);
+
+    days = days < 10 ? `0${days}` : days;
+    hours = hours < 10 ? `0${hours}` : hours;
+    minutes = minutes < 10 ? `0${minutes}` : minutes;
+    seconds = seconds < 10 ? `0${seconds}` : seconds;
 
     spanDay.textContent = days;
     spanHour.textContent = hours;
