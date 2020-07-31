16a638ebc94b86d6be3d3e4930410c334d6fee76
Daria Czerniawko <daria@czerniawko.pl>
2020-07-31 10:53:32 +0200

    added button

--- entry-form-in-React/js/app.js
+++ entry-form-in-React/js/app.js
@@ -1,38 +1,51 @@
 class CheckboxCoinfirmation extends React.Component {
   state = {
     isConfirmed: false,
+    isFormSubmitted: false,
   };
 
   handleCheckboxChange = () => {
     this.setState({
       isConfirmed: !this.state.isConfirmed,
+      isFormSubmitted: false,
     });
   };
 
+  handleFormSubmit = (e) => {
+    e.preventDefault();
+    this.setState({ isFormSubmitted: true });
+  };
+
   displayMessage = () => {
-    if (!this.state.isConfirmed) {
-      return <PositiveMessage />;
-    } else return <NegativeMessage />;
+    if (this.state.isFormSubmitted) {
+      if (!this.state.isConfirmed) {
+        return <PositiveMessage />;
+      } else return <NegativeMessage />;
+    }
   };
 
   render() {
     return (
       <>
-        <h1>Are you 21-years old?</h1>
-        <input
-          type="checkbox"
-          id="age"
-          onChange={this.handleCheckboxChange}
-          checked={this.state.isConfirmed}
-        />
-        <label htmlFor="age">Yes, I am!</label>
+        <h1> Are you 21 - years old ? </h1>
+        <form onSubmit={this.handleFormSubmit}>
+          <input
+            type="checkbox"
+            id="age"
+            onChange={this.handleCheckboxChange}
+            checked={this.state.isConfirmed}
+          />
+          <label htmlFor="age"> Yes, I am! </label>
+          <br /> <br />
+          <button type="submit">I want to buy ticket!</button>
+        </form>
         {this.displayMessage()}
       </>
     );
   }
 }
 
-const PositiveMessage = () => <p>You can't come in! </p>;
-const NegativeMessage = () => <p>Welcame!</p>;
+const PositiveMessage = () => <p> You can 't come in! </p>;
+const NegativeMessage = () => <p> Welcame! </p>;
 
 ReactDOM.render(<CheckboxCoinfirmation />, document.getElementById("root"));
