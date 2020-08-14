4bed61ba8842cf9a6053cc81b5c5654cdce01309
Daria Czerniawko <daria@czerniawko.pl>
2020-08-14 12:01:57 +0200

    init

---
+++ used-nodejs-express-mongodb/routes/news.js
@@ -0,0 +1,9 @@
+var express = require("express");
+var router = express.Router();
+
+/* GET home page. */
+router.get("/", (req, res) => {
+  res.render("news", { title: "News" });
+});
+
+module.exports = router;
