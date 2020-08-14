7c01650c2e7b51900a5570f052e4c1fbd2019da3
Daria Czerniawko <daria@czerniawko.pl>
2020-08-14 13:39:46 +0200

    before add database

--- used-nodejs-express-mongodb/routes/admin.js
+++ used-nodejs-express-mongodb/routes/admin.js
@@ -1,5 +1,14 @@
 var express = require("express");
+const { NotExtended } = require("http-errors");
 var router = express.Router();
+
+router.all("*", (req, res, next) => {
+  if (!req.session.admin) {
+    res.redirect("login");
+    return;
+  }
+  next();
+});
 
 /* GET home page. */
 router.get("/", (req, res) => {
