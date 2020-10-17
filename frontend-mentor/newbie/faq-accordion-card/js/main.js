e7cc2cc5d89e1ad5d257240738b2be236e129b09
Daria Czerniawko <daria@czerniawko.pl>
2020-10-17 14:39:56 +0200

    Add rotate image in button

--- frontend-mentor/newbie/faq-accordion-card/js/main.js
+++ frontend-mentor/newbie/faq-accordion-card/js/main.js
@@ -6,6 +6,11 @@ for (const btn of buttons) {
 
 function showAnswer(e) {
   const child = e.currentTarget.parentElement.parentElement.children[1];
-  if (child.style.display == "block") child.style.display = "none";
-  else child.style.display = "block";
+  if (child.style.display == "block") {
+    child.style.display = "none";
+    e.currentTarget.style.transform = "rotateX(0)";
+  } else {
+    child.style.display = "block";
+    e.currentTarget.style.transform = "rotateX(180deg)";
+  }
 }
