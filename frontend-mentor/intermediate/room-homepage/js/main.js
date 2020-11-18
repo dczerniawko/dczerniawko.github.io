97d270497a2b51ab41d885e8a73f7cd3575e08e2
Daria Czerniawko <daria@czerniawko.pl>
2020-11-18 17:31:06 +0100

    Onclick function button in JavaScript

--- frontend-mentor/intermediate/room-homepage/js/main.js
+++ frontend-mentor/intermediate/room-homepage/js/main.js
@@ -1,23 +1,23 @@
 const slideList = [
   {
-    h1: "Discover innovative ways to decorate",
-    text: `"We provide unmatched quality, comfort, and style for property owners across the country. 
+    title: "Discover innovative ways to decorate",
+    text: `We provide unmatched quality, comfort, and style for property owners across the country. 
             Our experts combine form andnfunction in bringing your vision to life. Create a room in your 
-            own style with our collection and make your property a reflection of you and what you love."`,
+            own style with our collection and make your property a reflection of you and what you love.`,
     photoSrc: "images/desktop-image-hero-1.jpg",
   },
   {
-    h1: "We are available all across the globe",
-    text: `"With stores all over the world, it's easy for you to find furniture for your home or place of business. 
+    title: "We are available all across the globe",
+    text: `With stores all over the world, it's easy for you to find furniture for your home or place of business. 
                 Locally, we’re in most major cities throughout the country. Find the branch nearest you using our 
-                store locator. Any questions? Don't hesitate to contact us today."`,
+                store locator. Any questions? Don't hesitate to contact us today.`,
     photoSrc: "images/desktop-image-hero-2.jpg",
   },
   {
-    h1: `"Manufactured with the best materials"`,
-    text: `"Our modern furniture store provide a high level of quality. Our company has invested in advanced technology 
+    title: "Manufactured with the best materials",
+    text: `Our modern furniture store provide a high level of quality. Our company has invested in advanced technology 
                 to ensure that every product is made as perfect and as consistent as possible. With three decades of 
-                experience in this industry, we understand what customers want for their home and office."`,
+                experience in this industry, we understand what customers want for their home and office.`,
     photoSrc: "images/desktop-image-hero-3.jpg",
   },
 ];
@@ -27,8 +27,9 @@ let slideActive = 0;
 const prevBtn = document.querySelector(".prev-btn");
 const nextBtn = document.querySelector(".next-btn");
 
+const title = document.querySelector("h1");
 const text = document.querySelector(".row-top__right__text");
-const photo = document.querySelector(".photo");
+const photo = document.querySelector(".row-top__left_img");
 
 const changeSlide = (e) => {
   if (e.currentTarget.className === "next-btn") slideActive++;
@@ -37,9 +38,8 @@ const changeSlide = (e) => {
   else if (slideActive < 0) slideActive = slideList.length - 1;
 
   text.textContent = slideList[slideActive].text;
-  name.textContent = slideList[slideActive].name;
-  position.textContent = slideList[slideActive].position;
   photo.setAttribute("src", slideList[slideActive].photoSrc);
+  title.textContent = slideList[slideActive].title;
 };
 
 nextBtn.addEventListener("click", changeSlide);
